1e6fa576bda95cf613c194177d71d255cf0a9dc5
Fábio Pereira <fabiopereira.fps@gmail.com>
2017-06-28 07:56:40 -0300

    Remove comments

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const logger = require('morgan')
 
-// Set up a new express app
 const app = express()
 
 app.use(logger('dev'))
 
-// Set up the routes
 app.use('/api', require('./routes'))
 
 app.listen(process.env.PORT, () => {
