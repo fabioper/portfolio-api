9db219843f8423a8df68f7557eab25dad08475c9
Fábio Pereira <fabiopereira.fps@gmail.com>
2017-09-02 03:08:24 -0300

    Add Project model

---
+++ models/project.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProjectSchema = new Schema({
+    _id: String,
+    title: String,
+    description: String,
+    roles: [String],
+    lessonsLearned: [String],
+    images: [String]
+}, {
+    timestamps: true
+});
+
+const Project = mongoose.model('project', ProjectSchema);
+
+module.exports = Project;
--- routes/projects.js
+++ routes/projects.js
@@ -1,17 +1,3 @@
-const router = require('express').Router()
-const bodyParser = require('body-parser')
-const Project = require('../models/project')
+const router = require('express').Router();
 
-router.use(bodyParser.urlencoded({ extended: false }))
-router.use(bodyParser.json())
-
-router.route('/')
-    .get()
-    .post()
-
-router.route('/:id')
-    .get()
-    .put()
-    .delete()
-
-module.exports = router
+module.exports = router;
