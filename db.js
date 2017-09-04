ba92c31940986d949d3fecc0ec936ab2a04fcb44
Fábio Pereira <fabiopereira.fps@gmail.com>
2017-09-04 20:54:03 -0300

    Isolate db connection on a separate file

---
+++ db.js
@@ -0,0 +1,8 @@
+const config = require('./config').get(process.env.NODE_ENV)
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(config.mongoUri, {
+    useMongoClient: true
+})
