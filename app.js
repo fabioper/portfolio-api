a4fb3f499e809eb3cd8527510d6409a732aa6028
Fábio Pereira <fabiopereira.fps@gmail.com>
2017-09-09 04:03:08 -0300

    Move errorHandler to a separate file

--- app.js
+++ app.js
@@ -1,36 +1,21 @@
 const express = require('express')
 const logger = require('morgan')
 const queryParser = require('./helpers/queryParser')
+const errorHandler = require('./helpers/errorHandler')
 
 const app = express()
 
 const port = process.env.PORT || 3001
 
+app.set('query parser', queryParser)
+
 if (process.env.NODE_ENV !== 'test') {
     app.use(logger('dev'))
 }
 
-app.set('query parser', queryParser)
-
 app.use('/api', require('./routes'))
 
-app.use((error, req, res, next) => {
-    let code = 500
-
-    if (error.name === 'ValidationError') {
-        code = 400
-    } else if (error.name === 'CastError') {
-        code = 404
-    }
-
-    res.status(code).json({
-        error: {
-            status: res.statusCode,
-            message: error.message,
-            type: error.name
-        }
-    })
-})
+app.use(errorHandler)
 
 app.listen(port, () => {
     if (process.env.NODE_ENV !== 'test') {
